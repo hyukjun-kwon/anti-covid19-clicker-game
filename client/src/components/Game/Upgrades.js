f23dd30596337fff7e79ab4206621c6bdb9469d5
jdab2020 <jdabalos90@gmail.com>
2020-03-30 17:56:34 -0700

    add component skeleton

---
+++ client/src/components/Game/Upgrades.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+function Uprgrades() {
+    return (
+        <Fragment>
+            <h1>Upgrades panel</h1>
+            <h1>Scrollable (up down, left right)</h1>
+        </Fragment>
+    )
+}
---
+++ client/src/components/HowToPlay/HowToPlay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HowToPlay() {
+    return <h1>How To Play</h1>
+}
+
+export default HowToPlay;
