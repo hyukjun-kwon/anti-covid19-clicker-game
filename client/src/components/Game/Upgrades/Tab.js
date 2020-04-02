bc7c0fff50329d56372cd45e836596d906358681
jdab2020 <jdabalos90@gmail.com>
2020-04-02 11:37:04 -0700

    reorganizing components

---
+++ client/src/components/Game/Upgrades/Tab.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function UpgradeTab() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper square className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="secondary"
+        aria-label="icon label tabs example"
+      >
+        <Tab label="Clicker" />
+        <Tab label="Facilities" />
+        <Tab label="Specials" />
+      </Tabs>
+    </Paper>
+  );
+}
