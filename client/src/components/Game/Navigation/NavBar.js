ba002630cff06f4bf5cb987df3d1a4e4bcc48692
jdab2020 <jdabalos90@gmail.com>
2020-04-02 14:20:51 -0700

    dry-ed up main.js

--- client/src/components/Game/Navigation/NavBar.js
+++ client/src/components/Game/Navigation/NavBar.js
@@ -4,16 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-// import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {flexGrow: 1,},
-//   menuButton: {marginRight: theme.spacing(2),},
-//   title: {flexGrow: 1,},
-// }));
-
 import Menu from './Menu'
 
 const useStyles = makeStyles((theme) => ({
@@ -46,24 +36,3 @@ function NavBar () {
 }
 
 export default NavBar;
-
-// export default function NavBar() {
-  
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//       <AppBar position="static" color="secondary">
-//         <Toolbar>
-//           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" className={classes.title} style={{textAlign: 'center'}}>
-//             COVID-19
-//           </Typography>
-//           <Button color="inherit">Login</Button>
-//         </Toolbar>
-//       </AppBar>
-//     </div>
-//   );
-// }
