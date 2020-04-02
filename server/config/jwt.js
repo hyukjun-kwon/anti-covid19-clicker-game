fad6de2475ea6142d9026d3f6ebfc2fc769c9a2b
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-04-02 10:36:59 -0700

    removing server routes temporarily

--- server/config/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-const { SECRET } = process.env;
-const ONE_DAY = "1d";
-
-module.exports = {
-  secret: SECRET,
-  sign: payload => jwt.sign(payload, SECRET, { expiresIn: ONE_DAY })
-};
