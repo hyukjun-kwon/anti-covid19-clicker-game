39417c8030d7dbc23513d23d86c214941cb74e65
jdab2020 <jdabalos90@gmail.com>
2020-04-02 14:13:25 -0700

    modified states | added states to parent app.js

--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -3,32 +3,23 @@ import Container from '@material-ui/core/Container';
 import Upgrades from './Upgrades/Upgrades';
 import NavBar from './Navigation/NavBar';
 import Main from './Main/Main';
-import PandemicContext from '../../contexts/PandemicContext';
 
-function Game() {
-    const [pandemic, setPandemic] = useState({
-        infected: 5000,
-        infectionRate: 1.05,
-        cured: 0,
-        dead: 0,
-        funding: 2500
-    })
+function Game( {setPandemic}) {
+
 
     return (
         <Fragment>
             <NavBar />
-            <PandemicContext.Provider value={pandemic}>
-                <Container>
+             <Container>
                     <Container>
-                        <Main setPandemic={setPandemic} />
+                        <Main setPandemic={setPandemic}/>
                     </Container>
                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                     <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
                     <Container>
-                        <Upgrades setPandemic={setPandemic} />
+                        <Upgrades />
                     </Container>
                 </Container>
-            </PandemicContext.Provider>
         </Fragment>
 
 
--- client/src/components/Game/Main/Main.js
+++ client/src/components/Game/Main/Main.js
@@ -1,70 +1,124 @@
-import React, { Fragment } from 'react';
-import { Box } from '@material-ui/core';
+import React, { Fragment, useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Grid, Paper, Typography } from '@material-ui/core';
 import Clicker from './Clicker';
+import PandemicContext from '../../../contexts/PandemicContext';
 
-function Main() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    Box: {
+        padding: theme.spacing(1),
+        textAlign: 'left',
+        width: '100%',
+        padding: '3px',
+        marginTop: '15px',
+
+
+    },
+}));
+
+function Main({setPandemic}) {
+    const pandemic = useContext(PandemicContext);
+    const classes = useStyles();
+
     return (
-        <Fragment>
-            <Box
-                style={{
-                    border: 'white 2px solid',
-                    width: '13%',
-                    background: 'black',
-                    color: 'white',
-                    textAlign: 'center',
-                    float: 'left',
-                    padding: '3px',
-                    marginTop: '15px',
-                    marginBottom: '15px',
-                    marginLeft: '390px'
-                }}>
-                <p>Vaccines: #vaccines </p>
-            </Box>
-            <Box
-                style={{
-                    border: 'white 2px solid',
-                    width: '13%',
-                    background: 'black',
-                    color: 'white',
-                    textAlign: 'center',
-                    float: 'left',
-                    padding: '3px',
-                    marginTop: '15px',
-                    marginBottom: '15px',
-                    marginLeft: '30px'
-                }}>
-                <p>Infected: #infected </p>
-            </Box>
-            <Box
-                style={{
-                    border: 'white 2px solid',
-                    width: '13%',
-                    background: 'black',
-                    float: 'left',
-                    color: 'white',
-                    textAlign: 'center',
-                    padding: '3px',
-                    marginTop: '15px',
-                    marginLeft: '30px'
-                }}>
-                <p>Dead: #dead </p>
-            </Box>
-            <Box
-                style={{
-                    border: 'white 2px solid',
-                    width: '13%',
-                    background: 'black',
-                    color: 'white',
-                    textAlign: 'center',
-                    float: 'left',
-                    padding: '3px',
-                    marginTop: '15px',
-                    marginLeft: '30px'
-                }}>
-                <p>Money: #money </p>
-            </Box>
-            <Clicker />
-        </Fragment>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={3}>
+                    <Box className={classes.Box} style={{ color: 'orange' }}>Infected: {pandemic.infected}</Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box className={classes.Box} style={{ color: 'red' }}>Dead: {pandemic.dead}</Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box className={classes.Box} style={{ color: 'blue' }}>Cured: {pandemic.cured}</Box>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box className={classes.Box} style={{ color: 'green' }}>Funding: {pandemic.funding}</Box>
+                </Grid>
+            </Grid>
+            <Clicker setPandemic={setPandemic}/>
+        </div>
+
+
+
+
+
+        // <Fragment>
+        //     <Grid container spacing={3}>
+        //         <Grid item xs={3}>
+        //             <Box
+        //                 style={{
+        //                     // border: 'white 2px solid',
+        //                     width: '100%',
+        //                     background: 'black',
+        //                     color: 'red',
+        //                     textAlign: 'center',
+        //                     float: 'left',
+        //                     padding: '3px',
+        //                     marginTop: '15px',
+        //                     marginBottom: '15px',
+        //                     marginLeft: '30px'
+        //                 }}>
+        //                 <p>Infected: {pandemic.infected} </p>
+        //             </Box>
+        //         </Grid>
+        //         <Grid item xs={3}>
+        //             <Box
+        //                 style={{
+        //                     // border: 'white 2px solid',
+        //                     width: '100%',
+        //                     background: 'black',
+        //                     float: 'left',
+        //                     color: 'grey',
+        //                     textAlign: 'center',
+        //                     padding: '3px',
+        //                     marginTop: '15px',
+        //                     marginLeft: '30px'
+        //                 }}>
+        //                 <p>Dead: {pandemic.dead} </p>
+        //             </Box>
+        //         </Grid>
+        //         <Grid item xs={3}>
+        //             <Box
+        //                 style={{
+        //                     // border: 'white 2px solid',
+        //                     width: '100%',
+        //                     background: 'black',
+        //                     color: 'green',
+        //                     textAlign: 'center',
+        //                     float: 'left',
+        //                     padding: '3px',
+        //                     marginTop: '15px',
+        //                     marginBottom: '15px',
+        //                     marginLeft: '390px'
+        //                 }}>
+        //                 <p>Cured: {pandemic.cured} </p>
+        //             </Box>
+        //         </Grid>
+        //         <Grid item xs={3}>
+        //             <Box
+        //                 style={{
+        //                     // border: 'white 2px solid',
+        //                     width: '100%',
+        //                     background: 'black',
+        //                     color: 'yellow',
+        //                     textAlign: 'center',
+        //                     float: 'left',
+        //                     padding: '3px',
+        //                     marginTop: '15px',
+        //                     marginLeft: '30px'
+        //                 }}>
+        //                 <p>Funding: {pandemic.funding} </p>
+        //             </Box>
+        //         </Grid>
+        //         <Grid item xs={12}>
+        //             <Clicker />
+        //         </Grid>
+        //     </Grid>
+        // </Fragment>
 
     )
 }
