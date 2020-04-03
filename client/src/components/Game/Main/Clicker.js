1eb3f1246fc6d4df55dd63410a6fa62d16e79bb8
Adam Greenthal <adamgreenthal@gmail.com>
2020-04-02 17:05:08 -0700

    commit

--- client/src/components/Game/Main/Clicker.js
+++ client/src/components/Game/Main/Clicker.js
@@ -1,35 +1,48 @@
 import React, { useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import ClickBtn from '../../../Images/clicker.png';
-import PandemicContext from '../../../contexts/PandemicContext'
+import PandemicContext from '../../../contexts/PandemicContext';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Clicker( { setPandemic } ) {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    HowTo: {
+        position: 'absolute',
+        left: '50%',
+        top: '42%',
+        transform: 'translate(-50%, -50%)',
+        marginBottom: '10%',
+        width: 'auto'
+    },
+    Clicker: {
+        height: '200px',
+        width: 'auto',
+        borderRadius: '50%'
+    },
+    ClickerBtn: {
+        position: 'absolute',
+        left: '49.7%',
+        top: '45%',
+        transform: 'translate(-50%, -50%)'
+    }
+}));
+
+function Clicker({ setPandemic }) {
+    const classes = useStyles();
     const pandemic = useContext(PandemicContext);
     const handleClick = event => {
         event.preventDefault();
         setPandemic({
             ...pandemic,
-            cured: pandemic.cured+5,
-            infected: pandemic.infected-5,
+            cured: pandemic.cured + 5,
+            infected: pandemic.infected - 5,
         })
     }
     return (
-        <Button 
-            style={{
-                position: 'absolute',
-                left: '49.7%',
-                top: '45%',
-                transform: 'translate(-50%, -50%)'
-        }} onClick={handleClick}>
-            <img 
-            src={ClickBtn} 
-            alt='clickerbtn'
-            style={{
-                height: '300px',
-                width: 'auto',
-                borderRadius: '50%'
-            }}>
-            </img>
+        <Button className={classes.ClickerBtn} onClick={handleClick}>
+            <img className={classes.Clicker} src={ClickBtn} alt='clickerbtn'></img>
         </Button>
     )
 }
