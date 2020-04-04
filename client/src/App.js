10bb03bace4b46e7e9b85765af397c6cd698eb64
jdab2020 <jdabalos90@gmail.com>
2020-04-04 16:15:24 -0700

    overhauled app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
@@ -7,7 +7,6 @@ import LogIn from './components/LogIn/LogInForm';
 import Home from './components/Home/Home';
 import HowToPlay from './components/HowToPlay/HowToPlay';
 import Game from './components/Game/Game';
-import PandemicContext from './contexts/PandemicContext';
 import Landing from '../src/components/Landing/Landing';
 import Register from '../src/components/Register/Form';
 
@@ -15,27 +14,19 @@ import { PandemicProvider } from "./contexts/PandemicContext";
 
 
 function App() {
-  const [pandemic, setPandemic] = useState({
-    difficulty:"easy",
-    infected: 5000,
-    infectionRate: 1.05,
-    cured: 0,
-    dead: 0,
-    funding: 2500
-  })
 
   return (
     <Router>
       <div>
-        <PandemicContext.Provider value={pandemic} >
+        <PandemicProvider>
           <Route exact path='/' component={LogIn} />
-          <Route exact path='/home' render={(props) => <Home {...props} setPandemic={setPandemic} />} />
+          <Route exact path='/home' component={Home} />
           <Route exact path='/how-to-play' component={HowToPlay} />
-          <Route exact path='/game' render={(props) => <Game {...props} setPandemic={setPandemic} />} />
+          <Route exact path='/game' component={Game} />
           <Route exact path='/logout' component={Landing} />
           <Route exact path='/login' component={LogIn} />
           <Route exact path='/register' component={Register} />
-        </PandemicContext.Provider>
+        </PandemicProvider>
       </div>
     </Router>
   );
